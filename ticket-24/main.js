d8e2c05cbf8e4a48e03df7293f2e777b352c237c
Gabriel Jefferson <phantom8528@gmail.com>
2022-04-15 09:07:40 -0400

    GabrielJefferson-Ticket31: linked weather icons to application, created breakpoints for mobile and tablet

--- ticket-24/main.js
+++ ticket-24/main.js
@@ -35,111 +35,248 @@ TrimbleMaps.APIKey = trimbleKey;
 // let searchBtn = document.getElementById('search-button');
 // searchBtn.addEventListener( `click`, chooseLocation);
 let weatherCity = document.querySelector('#city');
+let weatherStateRegion = document.querySelector('#state-region');
 let weatherDateTime = document.querySelector('#date-time');
 // const currentDateTime = new Date();
 let weatherTemperature = document.querySelector('#temperature');
-// let weatherIcon = document.getElementById('#condition-icon');
+let weatherIconBlock = document.getElementById('weather-icon-block');
+let weatherIcon = document.getElementById('condition-icon');
 
+let forecastHigh = document.querySelector('#forecast-high');
+let forecastLow = document.querySelector('#forecast-low');
+let forecastAvg = document.querySelector('#forecast-avg');
+let chanceOfRain = document.querySelector('#chance-of-rain');
+let rainInches = document.querySelector('#total-precip-in');
+let maxWindSpeed = document.querySelector('#max-wind');
 
 
 //-------------------------------FUNCTION(S)---------------------------
-function defaultWeather(dataPoint){
-    switch(dataPoint){
-        case "default":
-            //City of Atlanta
-            //--------------------------CITY, STATE / REGION, TEMP.IN F----------------------------------
-            fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=Atlanta`).then(function(response){
-            return response.json();
-                }).then(function(data){
-                    console.log('Success - This is For The Default City');
+function defaultWeather(){
+//--------------------------START OF FETCH-------------------------------------------
+fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=Atlanta`).then(function(response){
+    return response.json();
+        }).then(function(data){
+            console.log('Success - This is For The Default City');
 
-                    weatherCity.innerHTML = `${data.location.name}`;
-                    weatherDateTime.innerHTML = `${data.location.localtime}`;
-                    weatherTemperature.innerHTML = `${data.current.temp_f}`;
+            // let iconFromAPI = '//cdn.weatherapi.com/weather/64x64/day/116.png'
+            //data.current.condition.text
+            console.log(data.current.condition.icon); 
 
-                    let long = data.location.lon;
-                    let lat = data.location.lat;
 
-                    //-----------------------------TRIMBLE MAPS 1 - START---------------------------
-                    //NOTE: Dimensions of Map Are: (height: 377.99px, width: 444px)
+            weatherCity.innerHTML = `${data.location.name}`;
+            weatherStateRegion.innerHTML = `${data.location.region}`;
+            weatherDateTime.innerHTML = `${data.location.localtime}`;
+            weatherTemperature.innerHTML = `${data.current.temp_f}`;
 
-                    var myMap = new TrimbleMaps.Map({
-                        container: "myMap",
-                        // center: new TrimbleMaps.LngLat(-98.38, 38.69),
-                        center: new TrimbleMaps.LngLat(long, lat),
-                        style: TrimbleMaps.Common.Style.TRANSPORTATION,
-                        zoom: 10,
-                    });
-                    //----------------weather visibility------------------
 
-                    function switchLayer(layerId) {
-                        if (layerId === 'weather_radar') {
-                            myMap.setWeatherRadarVisibility(true);
-                        } else {
-                            myMap.setWeatherRadarVisibility(false);
-                        }
-                        if (layerId === 'weather_cloud') {
-                            map.setWeatherCloudVisibility(true);
-                        } else {
-                            myMap.setWeatherCloudVisibility(false);
-                        }
+
+            //---------START----------Comparing Icons on Local Machine with Icons Supplied by Api------
+            //1. Declare a variable assigned to the icon url provided by the api
+            let iconFromAPI = data.current.condition.icon;
+            //2. Declare two variables assigned to the unique day and nigttime substrings of the api urls
+            let dayIconSubstringFromApi = iconFromAPI.substring(35,46);
+            let nightIconSubstringFromApi = iconFromAPI.substring(35,48);
+            //3.Assign local machine urls that have been modified with interpolated substrings to their own variables
+            let daySource = `/Users/corcoding/Desktop/projects/frontend_repo/ticket-24/weather-icons/weather/64x64/${dayIconSubstringFromApi}`;
+            let nightSource = `/Users/corcoding/Desktop/projects/frontend_repo/ticket-24/weather-icons/weather/64x64/${nightIconSubstringFromApi}`;
+            //4. Use the length of the url for the api icons to determine if its day or night
+            if (iconFromAPI.length === 48){
+                //Nighttime icons | length of api url is 48
+                weatherIcon.src = nightSource;
+            }else{
+                //Day time icons | length of api url is 46
+                weatherIcon.src = daySource;
+            }
+            //---------END----------Comparing Icons on Local Machine with Icons Supplied by Api-------
+
+
+
+            let long = data.location.lon; //<-- Longitude of a Given Location
+            let lat = data.location.lat; //<-- Latitude of a Given Location
+
+            //-----------------------------TRIMBLE MAPS 1 - START---------------------------
+            //NOTE: Dimensions of Map Are: (height: 377.99px;, width: 444px;)<-- DESKTOP
+
+            var myMap = new TrimbleMaps.Map({
+                container: "myMap",
+                // center: new TrimbleMaps.LngLat(-98.38, 38.69),
+                center: new TrimbleMaps.LngLat(long, lat),
+                style: TrimbleMaps.Common.Style.TRANSPORTATION,
+                zoom: 10,
+            });
+            //----------------weather visibility------------------
+
+            function switchLayer(layerId) {
+                if (layerId === 'weather_radar') {
+                    myMap.setWeatherRadarVisibility(true);
+                } else {
+                    myMap.setWeatherRadarVisibility(false);
+                }
+                if (layerId === 'weather_cloud') {
+                    myMap.setWeatherCloudVisibility(true);
+                } else {
+                    myMap.setWeatherCloudVisibility(false);
+                }
+            }
+
+            function clickHandler (elem) {
+                switchLayer(elem.target.value);
+            }
+
+            const layerList = document.getElementById('menu');
+            const inputs = layerList.getElementsByTagName('input');
+
+            myMap.on('load', function() {
+                for (const element of inputs) {
+                    element.onclick = clickHandler;
+                    if(element.checked === true) {
+                        switchLayer(element.id);
                     }
-        
-                    function clickHandler (elem) {
-                        switchLayer(elem.target.value);
-                    }
-        
-                    const layerList = document.getElementById('menu');
-                    const inputs = layerList.getElementsByTagName('input');
-        
-                    myMap.on('load', function() {
-                        for (const element of inputs) {
-                            element.onclick = clickHandler;
-                            if(element.checked === true) {
-                                switchLayer(element.id);
-                            }
-                        }
-                    });
-                    //----------------weather visibility------------------
-                    //-----------------------------TRIMBLE MAPS 1 - END---------------------------
+                }
+            });
+            //----------------weather visibility------------------
+            //-----------------------------TRIMBLE MAPS 1 - END---------------------------
+
+        }).catch((error)=> {console.log(`There's a Problem: ${error}`)});
+
+    //--------------------------DAY FORCAST------------------------------------------
+    fetch(`http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=Atlanta`).then(function(response){
+        return response.json();
+        }).then(function(data){
+            console.log(`Forecast Data is Now Available`);
+            console.log(data);
+            console.log(data.forecast.forecastday[0].day.maxtemp_f);
+            //NOTE: To Extract forecast data use: data.forecast.forecastday[0].day
+
+            forecastHigh.innerHTML = `High for the Day | ${data.forecast.forecastday[0].day.maxtemp_f}`;
+            forecastLow.innerHTML = `Low for the Day | ${data.forecast.forecastday[0].day.mintemp_f}`;
+            forecastAvg.innerHTML = `Today's Average | ${data.forecast.forecastday[0].day.avgtemp_f}`;
+            chanceOfRain.innerHTML = `Chance of Rain | ${data.forecast.forecastday[0].day.daily_chance_of_rain}%`;
+            rainInches.innerHTML = `Inches of Rain | ${data.forecast.forecastday[0].day.totalprecip_in} inches`;
+            maxWindSpeed.innerHTML = `Windspeed | ${data.forecast.forecastday[0].day.maxwind_mph} mph`;
 
 
-                }).catch((error)=> {console.log(`There's a Problem: ${error}`)});
+            // forecastHigh.innerHTML = `${data.forecast.forecastday[0].day}`;
 
-            //--------------------------DAY FORCAST------------------------------------------
-            fetch(`http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=Atlanta`).then(function(response){
-                return response.json();
-                }).then(function(data){
-                    console.log(`Forecast Data is Now Available`);
-                    console.log(data);
-
-
-
-            }).catch((error)=> {console.log(`Forecast Not Available. Here's Why: ${error}`)});
-        case "dealersChoice":
-            //A random city
-    }
+    }).catch((error)=> {console.log(`Forecast Not Available. Here's Why: ${error}`)});
+    //-------------------------------------END OF FETCH-------------------------------------
 }
 
-defaultWeather("default"); //<-- Default city when the app is locaded (Atlanta, GA).
-//------------------------------------------------------------------------------------------------------------------
-function chooseLocation(){
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+function chooseLocation() {
+
     let textInput = document.querySelector('#search-input'); //<-- dom for input from user
     let choice = textInput.value; //<-- extracted value for user input
-
+    //--------------------------START OF FETCH-------------------------------------------
     fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${choice}`).then(function(response){
         return response.json();
             }).then(function(data){
-                console.log('Success - This Is For The User Input');
-                console.log(data);
-                //------------------------------------------------------------
+                console.log('Success - This is For The Default City');
+    
                 weatherCity.innerHTML = `${data.location.name}`;
-                weatherDateTime.innerHTML = currentDateTime; //<-- Not part of the api
+                weatherStateRegion.innerHTML = `${data.location.region}`;
+                weatherDateTime.innerHTML = `${data.location.localtime}`;
                 weatherTemperature.innerHTML = `${data.current.temp_f}`;
-            }).catch((error)=> {console.log(`There's a Problem: ${error}`)});
-}
 
-chooseLocation(); //<-User Input
+
+                //---------START----------Comparing Icons on Local Machine with Icons Supplied by Api------
+                //1. Declare a variable assigned to the icon url provided by the api
+                let iconFromAPI2 = data.current.condition.icon;
+                //2. Declare two variables assigned to the unique day and nigttime substrings of the api urls
+                let dayIconSubstringFromApi = iconFromAPI2.substring(35,46);
+                let nightIconSubstringFromApi = iconFromAPI2.substring(35,48);
+
+                //3. Assign local machine urls that have been modified with interpolated substrings to their own variables
+                let daySource = `/Users/corcoding/Desktop/projects/frontend_repo/ticket-24/weather-icons/weather/64x64/${dayIconSubstringFromApi}`;
+                let nightSource = `/Users/corcoding/Desktop/projects/frontend_repo/ticket-24/weather-icons/weather/64x64/${nightIconSubstringFromApi}`;
+
+                //4. Use the length of the url for the api icons to determine if its day or night
+                if (iconFromAPI2.length === 48){
+                    //Nighttime icons | length of api url is 48
+                    weatherIcon.src = nightSource;
+                }else{
+                    //Day time icons | length of api url is 46
+                    weatherIcon.src = daySource;
+                    
+                }
+                //---------END----------Comparing Icons on Local Machine with Icons Supplied by Api-------
+
+    
+                let long = data.location.lon;
+                let lat = data.location.lat;
+    
+                //-----------------------------TRIMBLE MAPS 1 - START---------------------------
+                //NOTE: Dimensions of Map Are: (height: 377.99px, width: 444px)
+    
+                var myMap = new TrimbleMaps.Map({
+                    container: "myMap",
+                    // center: new TrimbleMaps.LngLat(-98.38, 38.69),
+                    center: new TrimbleMaps.LngLat(long, lat),
+                    style: TrimbleMaps.Common.Style.TRANSPORTATION,
+                    zoom: 10,
+                });
+                //----------------weather visibility------------------
+    
+                function switchLayer(layerId) {
+                    if (layerId === 'weather_radar') {
+                        myMap.setWeatherRadarVisibility(true);
+                    } else {
+                        myMap.setWeatherRadarVisibility(false);
+                    }
+                    if (layerId === 'weather_cloud') {
+                        myMap.setWeatherCloudVisibility(true);
+                    } else {
+                        myMap.setWeatherCloudVisibility(false);
+                    }
+                }
+    
+                function clickHandler (elem) {
+                    switchLayer(elem.target.value);
+                }
+    
+                const layerList = document.getElementById('menu');
+                const inputs = layerList.getElementsByTagName('input');
+    
+                myMap.on('load', function() {
+                    for (const element of inputs) {
+                        element.onclick = clickHandler;
+                        if(element.checked === true) {
+                            switchLayer(element.id);
+                        }
+                    }
+                });
+                //----------------weather visibility------------------
+                //-----------------------------TRIMBLE MAPS 1 - END---------------------------
+    
+            }).catch((error)=> {console.log(`There's a Problem: ${error}`)});
+    
+        //--------------------------DAY FORCAST------------------------------------------
+        fetch(`http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${choice}`).then(function(response){
+            return response.json();
+            }).then(function(data){
+                console.log(`Forecast Data is Now Available`);
+                console.log(data);
+                console.log(data.forecast.forecastday[0].day);
+                //NOTE: To Extract forecast data use: data.forecast.forcastday[0].day
+
+                forecastHigh.innerHTML = `High for the Day | ${data.forecast.forecastday[0].day.maxtemp_f}`;
+                forecastLow.innerHTML = `Low for the Day | ${data.forecast.forecastday[0].day.mintemp_f}`;
+                forecastAvg.innerHTML = `Today's Average | ${data.forecast.forecastday[0].day.avgtemp_f}`;
+                chanceOfRain.innerHTML = `Chance of Rain | ${data.forecast.forecastday[0].day.daily_chance_of_rain}%`;
+                rainInches.innerHTML = `Inches of Rain | ${data.forecast.forecastday[0].day.totalprecip_in} inches`;
+                maxWindSpeed.innerHTML = `Windspeed | ${data.forecast.forecastday[0].day.maxwind_mph} mph`;
+
+                
+    
+        }).catch((error)=> {console.log(`Forecast Not Available. Here's Why: ${error}`)});
+        //-------------------------------------END OF FETCH-------------------------------------
+}; //<-- End of Function
+
+defaultWeather();
+chooseLocation();
+
+
 
 
 
