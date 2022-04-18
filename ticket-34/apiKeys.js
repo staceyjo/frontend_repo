b848d99ba27378be350e97b56f77aab6f253d075
Gabriel Jefferson <phantom8528@gmail.com>
2022-04-18 14:16:04 -0400

    GabrielJefferson-Final: Minor changes to format and spelling in the html. Minor modifications to the css

--- ticket-34/apiKeys.js
+++ ticket-34/apiKeys.js
@@ -1,8 +1,20 @@
-//API Key for WeatherAPI.com 
-export let apiKey = 'b38cc93161464a2ab2e173922221104';
-//API Keys for ...
+//-------------------------------API KEY(S)---------------------------
+//API KEY for WeatherAPI.com: b38cc93161464a2ab2e173922221104
+let apiKey = 'b38cc93161464a2ab2e173922221104';
+// import apiKey from `./apiKeys.js`;
+
+//API Key for OpenWeather: 91071e4140cc9b2dc181a6d120e76cc8
+let apiKey2 = `91071e4140cc9b2dc181a6d120e76cc8`;
+
 //--> Weatherbit.io
 let apiKey3 = `3f9ee3f848f44739abf4ab9c3f6b5e3e`;
 
+//-->AerisWeather.com 
+//client id: 
+let aerisClientID = `yjOvEECDU5vQUHKAcsCdh`;
+//key: 
+let aerisKey = `W7h4ov51ktrAWwswXtVzz0bLZr6YsrHJlzTrZ8hn`;
 
-
+//-->Trimble Maps Free Trial API Key:  D8D9A146E2C2A24F9C05DF1ED6D40585
+let trimbleKey = `D8D9A146E2C2A24F9C05DF1ED6D40585`;
+TrimbleMaps.APIKey = trimbleKey;
